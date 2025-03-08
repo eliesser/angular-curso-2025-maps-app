5b591d272fa27011a512e35175fd3bd7fbfde9c4
Eliesser Freites <freiteseliesser@hotmail.com>
2025-03-08 17:05:11 +0100

    Add fullscreen map page with Mapbox integration and styling

--- src/app/pages/fullscreen-map-page/fullscreen-map-page.component.ts
+++ src/app/pages/fullscreen-map-page/fullscreen-map-page.component.ts
@@ -1,8 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, signal, viewChild } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import mapboxgl from 'mapbox-gl';
+import { DecimalPipe, JsonPipe } from '@angular/common';
+
+mapboxgl.accessToken = environment.mapboxKey;
 
 @Component({
   selector: 'app-fullscreen-map-page',
-  imports: [],
+  imports: [DecimalPipe, JsonPipe],
   templateUrl: './fullscreen-map-page.component.html',
+  styleUrls: [`./fullscreen-map-page.component.css`],
 })
-export class FullscreenMapPageComponent { }
+export class FullscreenMapPageComponent {
+  divElement = viewChild<ElementRef>('map');
+  map = signal<mapboxgl.Map | null>(null);
+
+  zoom = signal(14);
+  coordinates = signal({
+    lng: -74.5,
+    lat: 40,
+  });
+
+  async ngAfterViewInit() {
+    if (!this.divElement()?.nativeElement) return;
+
+    await new Promise((resolve) => setTimeout(resolve, 80));
+
+    const element = this.divElement()!.nativeElement;
+    const { lat, lng } = this.coordinates();
+
+    const map = new mapboxgl.Map({
+      container: element,
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center: [lng, lat],
+      zoom: this.zoom(),
+    });
+  }
+}
